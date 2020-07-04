800f7a73692694624b78037773bcb9912e63b7be
Blake <blakeironsdev@gmail.com>
2020-07-04 04:41:07 -0500

    Updates

---
+++ precache-manifest.143a9b924b384751575819350920b311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8743b7da72f18b842eacedd053e15097",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "6861c93e2722d1fbba12",
+    "url": "/react-test/static/css/main.5fb94534.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6861c93e2722d1fbba12",
+    "url": "/react-test/static/js/main.013bc310.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  },
+  {
+    "revision": "91364a2c58a983cc5ea2aa1c999ad54d",
+    "url": "/react-test/static/media/bb.photo.91364a2c.JPG"
+  }
+]);
